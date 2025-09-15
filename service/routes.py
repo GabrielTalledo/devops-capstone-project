6cd8d60e91c8690f1ce9124c00f4c47874f8b1e3
GabrielTalledo <a20211755@pucp.edu.pe>
2025-09-15 11:15:32 -0400

    fix: Weird blank spaces :p

--- service/routes.py
+++ service/routes.py
@@ -51,7 +51,6 @@ def create_accounts():
     account.create()
     message = account.serialize()
     location_url = url_for("get_accounts", account_id=account.id, _external=True)
-    location_url = "/"  # Remove once get_accounts has been implemented
     return make_response(
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
@@ -59,6 +58,7 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
+
 
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
